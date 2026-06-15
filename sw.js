30a0d96aa659d3dcd7c0aaa1456a524c9bad95b0
kirbyzzzz <karby.tom@gmail.com>
2026-06-16 02:21:00 +0300

    PWA (installable+offline), progress charts, plan flexibility

---
+++ sw.js
@@ -0,0 +1,18 @@
+const C='karbyfit-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>{}));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const cp=resp.clone();
+      caches.open(C).then(c=>c.put(e.request,cp));
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
